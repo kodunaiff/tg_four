188384a5646ad3073385cfd2e25021ec1a1d88d4
kodunaiff <kodunaiff@gmail.com>
2024-06-17 16:47:55 +0800

    initial commit

--- tg_pro.py
+++ tg_pro.py
@@ -1,8 +1,29 @@
+import re
 
 phrases_folder = 'quiz-questions'
 phrases = ['1vs1200.txt', '1vs1201.txt', '1vs1298.txt']
 
-
 with open(f"{phrases_folder}/{phrases[1]}", "r", encoding="KOI8-R") as file:
     file_contents = file.read()
-print(file_contents)
+
+quizzes = file_contents.split('\n'+'\n')
+
+
+quizs ={}
+total=0
+for number, quiz in enumerate(quizzes, start=1):
+    if 'Вопрос ' in quiz:
+        quiz = quiz.replace("\n", " ")
+        question = quiz.split(': ')
+        total+=1
+        quizs[f'Вопрос {total}']=question[1]
+
+    elif 'Ответ:' in quiz:
+        quiz = quiz.replace("\n", " ")
+        answer = quiz.split(': ')
+        quizs[f'Ответ {total}'] = answer[1]
+
+#print(quizs)
+for zz, uu in quizs.items():
+    print(zz, uu)
+
