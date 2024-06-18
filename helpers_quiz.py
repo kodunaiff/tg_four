83f2cadab865311bcbd5661775925d9a758c72e8
kodunaiff <kodunaiff@gmail.com>
2024-06-18 15:49:50 +0800

    add question for users

---
+++ helpers_quiz.py
@@ -0,0 +1,33 @@
+
+def add_quiz(file_contents):
+    quizzes = file_contents.split('\n' + '\n')
+    quizs = {}
+    total = 0
+    for number, quiz in enumerate(quizzes, start=1):
+        if 'Вопрос ' in quiz:
+            quiz = quiz.replace("\n", " ")
+            question = quiz.split(': ')
+            total += 1
+            quizs[f'Вопрос {total}'] = question[1]
+
+        elif 'Ответ:' in quiz:
+            quiz = quiz.replace("\n", " ")
+            answer = quiz.split(': ')
+            quizs[f'Ответ {total}'] = answer[1]
+
+    return quizs
+
+
+def main():
+    phrases_folder = 'quiz-questions'
+    phrases = ['1vs1200.txt', '1vs1201.txt', '1vs1298.txt']
+    with open(f"{phrases_folder}/{phrases[1]}", "r", encoding="KOI8-R") as file:
+        file_contents = file.read()
+
+    ffff = add_quiz(file_contents)
+    print(ffff)
+
+
+
+if __name__ == '__main__':
+    main()
--- tg_bot.py
+++ tg_bot.py
@@ -1,8 +1,13 @@
 import logging
+import random
+from typing import Union, List
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+import telegram
 from environs import Env
 
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram import Update, ForceReply, bot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
+from helpers_quiz import add_quiz
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
@@ -10,49 +15,50 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-
 def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
     user = update.effective_user
+    custom_keyboard = [['Новый вопрос', 'Сдаться'], ['Мой счет']]
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
     update.message.reply_markdown_v2(
         fr'Hi {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
-    )
+        reply_markup=reply_markup)
 
-def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+def question(update: Update, context: CallbackContext) -> None:
+    number = random.randint(1, 24)
+    question1 = context.bot_data['quest']
+    text_q = question1[f'Вопрос {number}']
+    update.message.reply_text(text_q)
+
 
 
 def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
     update.message.reply_text(update.message.text)
+
+def mess(update: Update, context: CallbackContext) -> None:
+    chat_id = 951680654
+    context.bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
 
 def main() -> None:
     env = Env()
     env.read_env()
     tg_token = env.str("TG_TOKEN")
 
+    phrases_folder = 'quiz-questions'
+    phrases = ['1vs1200.txt', '1vs1201.txt', '1vs1298.txt']
+    with open(f"{phrases_folder}/{phrases[1]}", "r", encoding="KOI8-R") as file:
+        file_contents = file.read()
+
     updater = Updater(tg_token)
-
-    # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
+    dispatcher.bot_data['quest'] = add_quiz(file_contents)
 
-    # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-
-    # on non command i.e message - echo the message on Telegram
+    dispatcher.add_handler(CommandHandler("mess", mess))
+    dispatcher.add_handler(MessageHandler(Filters.regex("^Новый вопрос$"), question))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
-    # Start the Bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
